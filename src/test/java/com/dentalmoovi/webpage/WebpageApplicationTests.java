8e30245c39b22e58ff30a2cbb7552467612866fd
Oscar Gómez <98911168+Oagomezr@users.noreply.github.com>
2023-04-01 17:23:39 -0500

    start project

---
+++ src/test/java/com/dentalmoovi/webpage/WebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentalmoovi.webpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
